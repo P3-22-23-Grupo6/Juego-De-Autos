f092e501c74dd7752ec4d558a23dd6487cdb8a08
Dewei Chen <deweiche@ucm.es>
2023-04-18 11:37:14 +0200

    Bat para copiar el bin , desactivado cabezera de precompilado

---
+++ Juego_de_autos/Empezar a jugar_Debug.bat
@@ -0,0 +1,9 @@
+set MotorBin=.\Motor\Bin
+set Bin=.\Bin
+
+msbuild Juego_de_autos.sln /p:configuration=Debug
+msbuild Juego_de_autos.sln /p:configuration=Release
+
+xcopy %MotorBin% %Bin% /h /i /c /k /e /r /y
+cd %Bin%
+start LM_Main_d.exe
