09e3dd4f9992681bc4c81611da517c0e04c0faa9
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2023-05-03 22:35:32 +0200

    Boton para cambiar de velocidad en menu

--- Juego_de_autos/src/cpp/SceneChangeButton.cpp
+++ Juego_de_autos/src/cpp/SceneChangeButton.cpp
@@ -3,6 +3,7 @@
 #include "RaceManager.h"
 #include "Scene.h"
 #include "UIImageLM.h"
+#include "UITextLM.h"
 #include "ScriptManager.h"
 using namespace LocoMotor;
 JuegoDeAutos::SceneChangeButton::SceneChangeButton()
@@ -20,12 +21,45 @@ void JuegoDeAutos::SceneChangeButton::Start()
 	RaceManager::gameModeVelocity = RaceManager::GameModeVelocity::high;
 
 	_raceButton = gameObject->GetScene()->GetObjectByName("raceButton")->GetComponent<UIImageLM>();
+	_speedButton = gameObject->GetScene()->GetObjectByName("speedButton")->GetComponent<UIImageLM>();
+	_speedText = _speedButton->gameObject->GetComponent<UITextLM>();
 
-	//_button = gameObject->GetComponent<UIImageLM>();
+	if (_speedButton != nullptr) {
+		_speedButton->CallOnClick([this]() {
+
+			ChangeVelocity();
+			});
+	}
+
 	if (_raceButton != nullptr) {
 		_raceButton->CallOnClick([]() {
 			ScriptManager::GetInstance()->LoadSceneFromFile("Assets/scene.lua");
 			std::cout << "hola\n";
 		});
+	}
+}
+
+void JuegoDeAutos::SceneChangeButton::ChangeVelocity()
+{
+	int speedIndex = (int)currentSpeed;
+	if (speedIndex + 1 < 3)
+		speedIndex++;
+	else speedIndex = 0;
+
+	currentSpeed = (SpeedType) speedIndex;
+
+	switch (currentSpeed)
+	{
+	case JuegoDeAutos::SceneChangeButton::low:
+		_speedText->ChangeText("100CC");
+		break;
+	case JuegoDeAutos::SceneChangeButton::middle:
+		_speedText->ChangeText("200CC");
+		break;
+	case JuegoDeAutos::SceneChangeButton::high:
+		_speedText->ChangeText("500CC");
+		break;
+	default:
+		break;
 	}
 }
--- Juego_de_autos/src/include/SceneChangeButton.h
+++ Juego_de_autos/src/include/SceneChangeButton.h
@@ -3,6 +3,7 @@
 
 namespace LocoMotor {
 	class UIImageLM;
+	class UITextLM;
 }
 namespace JuegoDeAutos {
 	class SceneChangeButton : public LocoMotor::Component {
@@ -12,9 +13,16 @@ namespace JuegoDeAutos {
 		void Start() override;
 
 
-
 	private:
+
+		void ChangeVelocity();
+
 		LocoMotor::UIImageLM* _raceButton;
+		LocoMotor::UIImageLM* _speedButton;
+		LocoMotor::UITextLM* _speedText;
+
+		enum SpeedType {low, middle, high};
+		SpeedType currentSpeed;
 	};
 }
 
