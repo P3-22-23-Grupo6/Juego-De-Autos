ae2cf8e3e505fc4073e5d54f281fbeba2f061ad9
Jacobo0601 <82810110+Jacobo0601@users.noreply.github.com>
2023-05-04 20:43:27 +0200

    IA Separacion Inicial funcional 1º parte

--- Juego_de_autos/src/cpp/EnemyAI.cpp
+++ Juego_de_autos/src/cpp/EnemyAI.cpp
@@ -83,7 +83,7 @@ void EnemyAI::Update(float dt) {
 	upVector = upVector * raycastDistance;
 	to = from - upVector;
 
-	gameObject->SetPosition(mySpline->Interpolate(timeStep));
+	gameObject->SetPosition(mySpline->Interpolate(timeStep) + gameObject->GetTransform()->GetRotation().Right() * startSeparation);
 	//rbComp->AddForce(mySpline->Interpolate(timeStep));
 	//gameObject->GetTransform()->SetPosition(gameObject->GetTransform()->GetPosition() + 
 	//										(mySpline->Interpolate(timeStep) - gameObject->GetTransform()->GetPosition()) * 0.2f);
@@ -97,9 +97,9 @@ void EnemyAI::Update(float dt) {
 	
 		// Si hay mucha diferencia entre los vectores UP del suelo y la nave
 		// Ignorarlo, esto bloquea el subirse a las paredes
-		float angle = n.Angle(gameObject->GetTransform()->GetRotation().Up());
-		if (angle > 0.9f)
-			return;
+		//float angle = n.Angle(gameObject->GetTransform()->GetRotation().Up());
+		//if (angle > 0.9f)
+		//	return;
 		
 		//Intensidad con la que se va a actualizar el vector normal del coche
 		float pitchIntensity = 1000;
@@ -111,10 +111,8 @@ void EnemyAI::Update(float dt) {
 		//LMVector3 hoverDisplacement = LMVector3(n.GetX() * hoverDist, n.GetY() * hoverDist, n.GetZ() * hoverDist);
 		//gameObject->GetTransform()->SetPosition(hitPos + hoverDisplacement + (mySpline->Interpolate(timeStep) - gameObject->GetTransform()->GetPosition()) * 0.2f);
 	}
-
-	//newPos = newPos + gameObject->GetTransform()->GetRotation().Right() * startSeparation;
 	////LookAt
-	gameObject->GetTransform()->LookAt(mySpline->Interpolate(timeStep + 0.005f));
+	gameObject->GetTransform()->LookAt(mySpline->Interpolate(timeStep + 0.005f) + gameObject->GetTransform()->GetRotation().Right() * startSeparation);
 	////Set Position
 	//double hoverDist = 12; // 7
 	//LMVector3 hoverDisplacement = LMVector3(n.GetX() * hoverDist, n.GetY() * hoverDist, n.GetZ() * hoverDist);
