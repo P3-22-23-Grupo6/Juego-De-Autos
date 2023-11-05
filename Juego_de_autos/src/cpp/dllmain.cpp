15369300b0faeeaebe96cd806dc372a6770b79a9
Jacobo0601 <jacoboal@ucm.es>
2023-11-05 18:23:02 +0100

    Arreglo DllMain

--- Juego_de_autos/src/cpp/dllmain.cpp
+++ Juego_de_autos/src/cpp/dllmain.cpp
@@ -46,7 +46,7 @@ extern "C" __declspec(dllexport) const char* InitJuego(LocoMotor::MotorApi* moto
 
     motor->RegisterGame("Juego de autos");
 
-    LocoMotor::ScriptManager::GetInstance()->LoadSceneFromFile("Assets/Scenes/SalaDebug.lua");
+    LocoMotor::ScriptManager::GetInstance()->LoadSceneFromFile("Assets/Scenes/intro.lua");
 
 #ifdef _DEBUG
     return "Juego de autos: Running in DEBUG";
